ad9fba06796ec2444aaa908bba30c98ae4874a33
Samuel Gfeller <samuelgfeller@bluewin.ch>
2019-09-29 21:30:57 +0200

    removed youtube api key

--- config/config.php
+++ config/config.php
@@ -1,6 +1,5 @@
 <?php
 return [
-//    'youtube_data_key' => 'AIzaSyDHGUTficv6Xp7z7Zdsc2a3DBTKhxRNc40',
     'download_directory' => 'downloads/',
     'video_extension' => '.mp4',
     'max_record_time' => 50
