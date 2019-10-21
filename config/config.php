76cb97c54773b61231dd06d2b87efda2f1e515a9
Samuel Gfeller <sgfeller@wmc.ch>
2019-10-21 18:43:36 +0200

    Fixed thumbnail image

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 return [
     'video_directory' => 'downloads/videos/',
     'thumbnail_directory' => 'downloads/thumbnails/',
-    'thumbnail_not_found_url' => 'https://imgur.com/zWoKvJ9',
+    'thumbnail_not_found_url' => 'https://imgur.com/zWoKvJ9.jpg',
     'video_extension' => '.mp4',
     'max_record_time' => 20,
     // domain with trailing slash
